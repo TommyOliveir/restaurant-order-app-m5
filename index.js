c04b9eb8184e97ad8c907d1f8b2b4160d5782a73
TommyOliveir <laridatommy@gmail.com>
2022-10-21 13:12:59 +0200

    commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { menuArray } from "./data.js"
 
+const OrderProcess = document.getElementById("order-process")
+
+
 document.addEventListener("click", function (e) {
 
     if (e.target.dataset.additemmenu) {
@@ -16,8 +19,20 @@ function handleAddMenuClick(menuId) {
     orderArray.push(order)
 
     getOrderHtml()
+   const totalsum = getSumPrice()
+   document.getElementById("total-price").innerHTML = `<p>Total Price</p> $${totalsum} `
+  
+   console.log(totalsum )
+   OrderProcess.classList.remove("hidden");
+   OrderProcess.classList.add("show");
 }
 
+function getSumPrice() {
+    const totalPrice = orderArray.reduce((total, order) => {
+        return total + order.price
+    }, 0)
+   return totalPrice
+}
 
 function AddMenu(addMenuId) {
     const targetMenuObj = menuArray.filter(function (addmenu) {
@@ -37,15 +52,17 @@ function getOrderHtml() {
     // }
     orderArray.forEach(function (order) {
         // console.log(order.name)
-        orderHtml += `
-                        <div>
+        orderHtml += ` 
+                        <div class="ordered-items">
                             <p>
                             ${order.name}
                             </p>
                             <p>
-                            ${order.price}
+                            $${order.price}
                             </p>
+                            
                         </div>
+                        <hr>
                     `
 
     })
@@ -86,6 +103,7 @@ function getMenuHtml() {
 
 function render() {
     document.getElementById("menu").innerHTML = getMenuHtml()
+    
 }
 
 render()
